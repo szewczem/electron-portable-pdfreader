e848807dbbd2266d7d5a471432afe575c7d96cbd
szewczem <szewczem@gmail.com>
2025-10-03 20:32:21 +0200

    iframe for pages in hash routing

---
+++ src/js/pages.js
@@ -0,0 +1,98 @@
+export const pages = {
+  TEST1_A: '<iframe class="pdf" src="./doc/section1/TEST1_A.pdf#page=1&view=fitV"></iframe>',
+  TEST1_B: '<iframe class="pdf" src="./doc/section1/TEST1_B.pdf#page=1&view=fitV"></iframe>',
+  TEST1_C: '<iframe class="pdf" src="./doc/section1/TEST1_C.pdf#page=1&view=fitV"></iframe>',
+  
+  TEST2_A: '<iframe class="pdf" src="./doc/section2/TEST2_A.pdf#page=1&view=fitV"></iframe>',
+  TEST2_B: '<iframe class="pdf" src="./doc/section2/TEST2_B.pdf#page=1&view=fitV"></iframe>',
+  TEST2_C: '<iframe class="pdf" src="./doc/section2/TEST2_C.pdf#page=1&view=fitV"></iframe>',
+
+  TEST3A_A: '<iframe class="pdf" src="./doc/section3/TEST3A_A.pdf#page=1&view=fitV"></iframe>',
+  TEST3A_B: '<iframe class="pdf" src="./doc/section3/TEST3A_B.pdf#page=1&view=fitV"></iframe>',
+  TEST3A_C: '<iframe class="pdf" src="./doc/section3/TEST3A_C.pdf#page=1&view=fitV"></iframe>',
+
+  TEST3B_A: '<iframe class="pdf" src="./doc/section3/TEST3B_A.pdf#page=1&view=fitV"></iframe>',
+  TEST3B_B: '<iframe class="pdf" src="./doc/section3/TEST3B_B.pdf#page=1&view=fitV"></iframe>',
+
+  TEST3C_A: '<iframe class="pdf" src="./doc/section3/TEST3C_A.pdf#page=1&view=fitV"></iframe>',
+  TEST3C_B: '<iframe class="pdf" src="./doc/section3/TEST3C_B.pdf#page=1&view=fitV"></iframe>',
+
+  TEST4A_A: '<iframe class="pdf" src="./doc/section4/TEST4A_A.pdf#page=1&view=fitV"></iframe>',
+  TEST4A_B: '<iframe class="pdf" src="./doc/section4/TEST4A_B.pdf#page=1&view=fitV"></iframe>',
+  TEST4A_C: '<iframe class="pdf" src="./doc/section4/TEST4A_C.pdf#page=1&view=fitV"></iframe>',
+
+  TEST4B_A: '<iframe class="pdf" src="./doc/section4/TEST4B_A.pdf#page=1&view=fitV"></iframe>',
+  TEST4B_B: '<iframe class="pdf" src="./doc/section4/TEST4B_B.pdf#page=1&view=fitV"></iframe>',
+
+  TEST4C_A: '<iframe class="pdf" src="./doc/section4/TEST4C_A.pdf#page=1&view=fitV"></iframe>',
+  TEST4C_B: '<iframe class="pdf" src="./doc/section4/TEST4C_B.pdf#page=1&view=fitV"></iframe>',
+
+  TEST4D_A: '<iframe class="pdf" src="./doc/section4/TEST4D_A.pdf#page=1&view=fitV"></iframe>',
+
+  TEST4E_A: '<iframe class="pdf" src="./doc/section4/TEST4E_A.pdf#page=1&view=fitV"></iframe>',
+  TEST4E_B: '<iframe class="pdf" src="./doc/section4/TEST4E_B.pdf#page=1&view=fitV"></iframe>',
+  TEST4E_C: '<iframe class="pdf" src="./doc/section4/TEST4E_C.pdf#page=1&view=fitV"></iframe>',
+
+  TEST5A_A: '<iframe class="pdf" src="./doc/section5/TEST5A_A.pdf#page=1&view=fitV"></iframe>',
+  TEST5A_B: '<iframe class="pdf" src="./doc/section5/TEST5A_B.pdf#page=1&view=fitV"></iframe>',
+  TEST5A_C: '<iframe class="pdf" src="./doc/section5/TEST5A_C.pdf#page=1&view=fitV"></iframe>',
+
+  TEST5B_A: '<iframe class="pdf" src="./doc/section5/TEST5B_A.pdf#page=1&view=fitV"></iframe>',
+  TEST5B_B: '<iframe class="pdf" src="./doc/section5/TEST5B_B.pdf#page=1&view=fitV"></iframe>',
+
+  TEST5C_A: '<iframe class="pdf" src="./doc/section5/TEST5C_A.pdf#page=1&view=fitV"></iframe>',
+  TEST5C_B: '<iframe class="pdf" src="./doc/section5/TEST5C_B.pdf#page=1&view=fitV"></iframe>',
+
+  TEST5D_A:  '<iframe class="pdf" src="./doc/section5/TEST5D_A.pdf#page=1&view=fitV"></iframe>',
+
+  TEST5E_A: '<iframe class="pdf" src="./doc/section5/TEST5E_A.pdf#page=1&view=fitV"></iframe>',
+  TEST5E_B: '<iframe class="pdf" src="./doc/section5/TEST5E_B.pdf#page=1&view=fitV"></iframe>',
+  TEST5E_C: '<iframe class="pdf" src="./doc/section5/TEST5E_C.pdf#page=1&view=fitV"></iframe>',
+
+  TEST5F_A: '<iframe class="pdf" src="./doc/section5/TEST5F_A.pdf#page=1&view=fitV"></iframe>',
+  TEST5F_B: '<iframe class="pdf" src="./doc/section5/TEST5F_B.pdf#page=1&view=fitV"></iframe>',
+  TEST5F_C: '<iframe class="pdf" src="./doc/section5/TEST5F_C.pdf#page=1&view=fitV"></iframe>',
+
+  TEST6A_A: '<iframe class="pdf" src="./doc/section6/TEST6A_A.pdf#page=1&view=fitV"></iframe>',
+  TEST6A_B: '<iframe class="pdf" src="./doc/section6/TEST6A_B.pdf#page=1&view=fitV"></iframe>',
+  TEST6A_C: '<iframe class="pdf" src="./doc/section6/TEST6A_C.pdf#page=1&view=fitV"></iframe>',
+
+  TEST6B_A: '<iframe class="pdf" src="./doc/section6/TEST6B_A.pdf#page=1&view=fitV"></iframe>',
+  TEST6B_B: '<iframe class="pdf" src="./doc/section6/TEST6B_B.pdf#page=1&view=fitV"></iframe>',
+  TEST6B_C: '<iframe class="pdf" src="./doc/section6/TEST6B_C.pdf#page=1&view=fitV"></iframe>',
+
+  TEST6C_A: '<iframe class="pdf" src="./doc/section6/TEST6C_A.pdf#page=1&view=fitV"></iframe>',
+  TEST6C_B: '<iframe class="pdf" src="./doc/section6/TEST6C_B.pdf#page=1&view=fitV"></iframe>',
+
+  TEST6D_A: '<iframe class="pdf" src="./doc/section6/TEST6D_A.pdf#page=1&view=fitV"></iframe>',
+
+  TEST7A_A: '<iframe class="pdf" src="./doc/section7/TEST7A_A.pdf#page=1&view=fitV"></iframe>',
+  TEST7A_B: '<iframe class="pdf" src="./doc/section7/TEST7A_B.pdf#page=1&view=fitV"></iframe>',
+  TEST7A_C: '<iframe class="pdf" src="./doc/section7/TEST7A_C.pdf#page=1&view=fitV"></iframe>',
+
+  TEST7B_A: '<iframe class="pdf" src="./doc/section7/TEST7B_A.pdf#page=1&view=fitV"></iframe>',
+  TEST7B_B: '<iframe class="pdf" src="./doc/section7/TEST7B_B.pdf#page=1&view=fitV"></iframe>',
+  TEST7B_C: '<iframe class="pdf" src="./doc/section7/TEST7B_C.pdf#page=1&view=fitV"></iframe>',
+
+  TEST7C_A: '<iframe class="pdf" src="./doc/section7/TEST7C_A.pdf#page=1&view=fitV"></iframe>',
+  TEST7C_B: '<iframe class="pdf" src="./doc/section7/TEST7C_B.pdf#page=1&view=fitV"></iframe>',
+
+  TEST7D_A: '<iframe class="pdf" src="./doc/section7/TEST7D_A.pdf#page=1&view=fitV"></iframe>',
+
+  TEST7E_A: '<iframe class="pdf" src="./doc/section7/TEST7E_A.pdf#page=1&view=fitV"></iframe>',
+  TEST7E_B: '<iframe class="pdf" src="./doc/section7/TEST7E_B.pdf#page=1&view=fitV"></iframe>',
+  TEST7E_C: '<iframe class="pdf" src="./doc/section7/TEST7E_C.pdf#page=1&view=fitV"></iframe>',
+  TEST7E_D: '<iframe class="pdf" src="./doc/section7/TEST7E_D.pdf#page=1&view=fitV"></iframe>',
+
+  TEST7F_A: '<iframe class="pdf" src="./doc/section7/TEST7F_A.pdf#page=1&view=fitV"></iframe>',
+  TEST7F_B: '<iframe class="pdf" src="./doc/section7/TEST7F_B.pdf#page=1&view=fitV"></iframe>',
+  TEST7F_C: '<iframe class="pdf" src="./doc/section7/TEST7F_C.pdf#page=1&view=fitV"></iframe>',
+  TEST7F_D: '<iframe class="pdf" src="./doc/section7/TEST7F_D.pdf#page=1&view=fitV"></iframe>',
+  TEST7F_E: '<iframe class="pdf" src="./doc/section7/TEST7F_E.pdf#page=1&view=fitV"></iframe>',
+
+  TEST7G_A: '<iframe class="pdf" src="./doc/section7/TEST7G_A.pdf#page=1&view=fitV"></iframe>',
+  TEST7G_B: '<iframe class="pdf" src="./doc/section7/TEST7G_B.pdf#page=1&view=fitV"></iframe>',
+
+  TEST7H_A: '<iframe class="pdf" src="./doc/section7/TEST7H_A.pdf#page=1&view=fitV"></iframe>',
+  TEST7H_B: '<iframe class="pdf" src="./doc/section7/TEST7H_B.pdf#page=1&view=fitV"></iframe>',
+};
